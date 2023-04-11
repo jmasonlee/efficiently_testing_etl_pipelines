ff414fbdb32e38691543eb05cb53c4bac816bacb
jmasonlee <jmasonlee@gmail.com>
2023-04-11 14:08:54 +0000

    t. str rep of df

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -4,6 +4,5 @@ from approvaltests import verify
 def test_will_replace_null_prices_with_floating_averages(spark) -> None:
     df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
     df = replace_null_prices_with_floating_averages(df)
-    df_columns = df.dtypes
-    df_columns = " | ".join([f"{column[0]}::{column[1]}" for column in df_columns])
-    verify(df_columns)
+    
+    verify(df.toPandas().to_string(header=True, index=False))
