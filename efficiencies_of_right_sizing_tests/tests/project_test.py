4925af2d661088c88b38adc3df41b8b0ff9d12b5
jmasonlee <jmasonlee@gmail.com>
2023-04-11 13:46:37 +0000

    t. Convert array to string

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -5,5 +5,5 @@ def test_will_replace_null_prices_with_floating_averages(spark) -> None:
     df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
     df = replace_null_prices_with_floating_averages(df)
     df_columns = df.dtypes
-    df_columns = [f"{column[0]}::{column[1]}" for column in df_columns]
+    df_columns = " | ".join([f"{column[0]}::{column[1]}" for column in df_columns])
     verify(df_columns)
