e356e8439c10f7db5e8a434486b3c6bb51be13b7
jmasonlee <jmasonlee@gmail.com>
2023-04-16 01:52:13 +0000

    R!! Prepare for extract

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -6,8 +6,8 @@ def test_will_replace_null_prices_with_floating_averages(spark) -> None:
     input_df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
     output_df = replace_null_prices_with_floating_averages(input_df)
     
-    test_case = f"{get_string_representation_of_dataframe(input_df)}\n------- WITH NULL PRICES REPLACED -------\n{get_string_representation_of_dataframe(output_df)}"
-    verify(test_case)
+    output_df_as_str = get_string_representation_of_dataframe(output_df)
+    verify(output_df_as_str)
 
 def get_string_representation_of_dataframe(df: DataFrame) -> str:
     return df.toPandas().to_string(header=True, index=False)
