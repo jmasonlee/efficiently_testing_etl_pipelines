10e2400027a985496c863feb122aee87ce7fff4d
jmasonlee <jmasonlee@gmail.com>
2023-04-16 01:44:56 +0000

    R. extract variable

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -3,10 +3,10 @@ from approvaltests import verify
 from pyspark.sql import DataFrame
 
 def test_will_replace_null_prices_with_floating_averages(spark) -> None:
-    df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
-    df = replace_null_prices_with_floating_averages(df)
+    input_df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
+    output_df = replace_null_prices_with_floating_averages(input_df)
     
-    verify(get_string_representation_of_dataframe(df))
+    verify(get_string_representation_of_dataframe(output_df))
 
 def get_string_representation_of_dataframe(df: DataFrame) -> str:
     return df.toPandas().to_string(header=True, index=False)
