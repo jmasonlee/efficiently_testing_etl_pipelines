d1f2441d901bcae4a87500d9263ba17e6e01e939
jmasonlee <jmasonlee@gmail.com>
2023-04-16 01:19:55 +0000

    F  add types to printer

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -1,8 +1,15 @@
 from src.project import replace_null_prices_with_floating_averages
 from approvaltests import verify
+from pyspark.sql import DataFrame
 
 def test_will_replace_null_prices_with_floating_averages(spark) -> None:
     df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
     df = replace_null_prices_with_floating_averages(df)
     
-    verify(df.toPandas().to_string(header=True, index=False))
+    verify(get_string_representation_of_dataframe(df))
+
+def get_string_representation_of_dataframe(df: DataFrame) -> str:
+    str_df = df.toPandas().to_string(header=True, index=False)
+    for name, dtype in df.dtypes:
+        str_df = str_df.replace(name, f"{name}::{dtype}")
+    return str_df
