3c6c0dc51183a9981d9995cc2bd72f95e68e6ee9
jmasonlee <jmasonlee@gmail.com>
2023-04-16 01:43:51 +0000

    t  Fix experiment in test

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -9,7 +9,5 @@ def test_will_replace_null_prices_with_floating_averages(spark) -> None:
     verify(get_string_representation_of_dataframe(df))
 
 def get_string_representation_of_dataframe(df: DataFrame) -> str:
-    str_df = df.toPandas().to_string(header=True, index=False)
-    for name, dtype in df.dtypes:
-        str_df = str_df.replace(name, f"{name}::{dtype}")
-    return str_df
+    return df.toPandas().to_string(header=True, index=False)
+    
