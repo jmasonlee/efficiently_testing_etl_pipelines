586236d9943f90e8db0b156733395060b071d75c
jmasonlee <jmasonlee@gmail.com>
2023-04-16 01:47:47 +0000

    F  Better representation of test case

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -6,7 +6,8 @@ def test_will_replace_null_prices_with_floating_averages(spark) -> None:
     input_df = spark.createDataFrame([{"price": 327, "cut": "good", "clarity": "SI2"}])
     output_df = replace_null_prices_with_floating_averages(input_df)
     
-    verify(get_string_representation_of_dataframe(output_df))
+    test_case = f"{get_string_representation_of_dataframe(input_df)}\n------- WITH NULL PRICES REPLACED -------\n{get_string_representation_of_dataframe(output_df)}"
+    verify(test_case)
 
 def get_string_representation_of_dataframe(df: DataFrame) -> str:
     return df.toPandas().to_string(header=True, index=False)
