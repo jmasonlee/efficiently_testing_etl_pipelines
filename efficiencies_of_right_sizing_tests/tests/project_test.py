395dee7889e4d54858a5a829e6e11820cac87f59
jmasonlee <jmasonlee@gmail.com>
2023-05-06 17:45:44 +0000

    R!! Cleaned up test file

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -1,38 +1,10 @@
-from src.project import replace_null_prices_with_floating_averages
-from typing import List
-from approvaltests import verify_all_combinations, Options
-from approvaltests.scrubbers.scrubbers import create_regex_scrubber
-from pyspark.sql import DataFrame, Row, SparkSession
-
-
-def verify_will_replace_null_values__with_floating_averages(
-    spark, price:List[float], cut: List[str] = [], clarity: List[str] = [], color: List[str] = []
-    ) -> None:
-    
-    arguments = [[spark], price]
-    if cut:
-        arguments.append(cut)
-    if clarity:
-        arguments.append(clarity)
-    if color:
-        arguments.append(color)
-    
-    return verify_all_combinations(impute_null_values_for_diamond, arguments, options=Options().with_scrubber(
-                create_regex_scrubber("<pyspark.sql.session.SparkSession object at (.*?)>", "[SparkSession]")
-            ))
+from pyspark.sql import SparkSession
+from efficiencies_of_right_sizing_tests.tests.test_magic_do_not_look.verification_helpers import verify_will_replace_null_values_with_floating_averages
 
 
 def test_will_replace_null_prices_with_floating_averages(spark: SparkSession) -> None:
     price = [327]
 
-    verify_will_replace_null_values__with_floating_averages(spark, price)
+    verify_will_replace_null_values_with_floating_averages(spark, price)
    
-
-def impute_null_values_for_diamond(spark: SparkSession, price:float, cut: str = "good", clarity: str = "SI2", color: str = "A") -> str:
-    input_df = spark.createDataFrame([
-        Row(id=1, price=price,cut=cut,clarity=clarity,color=color)
-    ])
-    output_df = replace_null_prices_with_floating_averages(input_df)
-    
-    return output_df.where(output_df.id==1).first()['price']
     
---
+++ efficiencies_of_right_sizing_tests/tests/test_magic_do_not_look/verification_helpers.py
@@ -0,0 +1,29 @@
+from src.project import replace_null_prices_with_floating_averages
+from typing import List
+from approvaltests import verify_all_combinations, Options
+from approvaltests.scrubbers.scrubbers import create_regex_scrubber
+from pyspark.sql import DataFrame, Row, SparkSession
+
+def verify_will_replace_null_values_with_floating_averages(
+    spark, price:List[float], cut: List[str] = [], clarity: List[str] = [], color: List[str] = []
+    ) -> None:
+    
+    arguments = [[spark], price]
+    if cut:
+        arguments.append(cut)
+    if clarity:
+        arguments.append(clarity)
+    if color:
+        arguments.append(color)
+    
+    return verify_all_combinations(impute_null_values_for_diamond, arguments, options=Options().with_scrubber(
+                create_regex_scrubber("<pyspark.sql.session.SparkSession object at (.*?)>", "[SparkSession]")
+            ))
+    
+def impute_null_values_for_diamond(spark: SparkSession, price:float, cut: str = "good", clarity: str = "SI2", color: str = "A") -> str:
+    input_df = spark.createDataFrame([
+        Row(id=1, price=price,cut=cut,clarity=clarity,color=color)
+    ])
+    output_df = replace_null_prices_with_floating_averages(input_df)
+    
+    return output_df.where(output_df.id==1).first()['price']
