fd6a33adcae5903966cc1fc2733ed35d668647e6
jmasonlee <jmasonlee@gmail.com>
2023-05-06 17:27:22 +0000

    F!! Refactor to base combinations

--- efficiencies_of_right_sizing_tests/tests/project_test.py
+++ efficiencies_of_right_sizing_tests/tests/project_test.py
@@ -1,21 +1,22 @@
 from src.project import replace_null_prices_with_floating_averages
-from approvaltests import verify
+from approvaltests import verify_all_combinations, Options
+from approvaltests.scrubbers.scrubbers import create_regex_scrubber
 from pyspark.sql import DataFrame, Row, SparkSession
 
 
 def test_will_replace_null_prices_with_floating_averages(spark: SparkSession) -> None:
-    output_df_as_str = impute_null_values_for_diamond(spark, price=327, cut = "good", clarity = "SI2")
+    price = [327]
 
-    verify(output_df_as_str)
+    verify_all_combinations(impute_null_values_for_diamond, [[spark], price], options=Options().with_scrubber(
+            create_regex_scrubber("<pyspark.sql.session.SparkSession object at (.*?)>", "[SparkSession]")
+        ))
+   
 
-def get_string_representation_of_dataframe(df: DataFrame) -> str:
-    return df.toPandas().to_string(header=True, index=False)
-
-def impute_null_values_for_diamond(spark: SparkSession, price:float, cut: str, clarity: str, color: str = "A") -> str:
+def impute_null_values_for_diamond(spark: SparkSession, price:float, cut: str = "good", clarity: str = "SI2", color: str = "A") -> str:
     input_df = spark.createDataFrame([
-        Row(price=price,cut=cut,clarity=clarity,color=color)
+        Row(id=1, price=price,cut=cut,clarity=clarity,color=color)
     ])
     output_df = replace_null_prices_with_floating_averages(input_df)
     
-    return get_string_representation_of_dataframe(output_df)
+    return output_df.where(output_df.id==1).first()['price']
     
