15ef845af727f478835759cad93c0eeef575a3ec
jmasonlee <jmasonlee@gmail.com>
2023-06-19 14:28:17 -0600

    t WTF but now I'm green

--- efficiencies_of_right_sizing_tests/src/linear_regression_prep.py
+++ efficiencies_of_right_sizing_tests/src/linear_regression_prep.py
@@ -2,13 +2,14 @@ from pyspark.sql import DataFrame
 from pyspark.sql.functions import log
 
 from efficiencies_of_right_sizing_tests.src.build_indep_vars import build_indep_vars
+from efficiencies_of_right_sizing_tests.src.diamond_pricing import replace_null_prices_with_floating_averages
 
 
 def transform(df: DataFrame) -> DataFrame:
-    df = df[['carat', 'clarity', 'color', 'price']]
 
     df = df.withColumn('lprice', log('price'))
     df = replace_null_prices_with_floating_averages(df)
+    df = df[['carat', 'clarity', 'color', 'price']]
     df = build_indep_vars(df, ['carat', 'clarity', 'color'],
                                       categorical_vars=['clarity', 'color'],
                                       keep_intermediate=False,
